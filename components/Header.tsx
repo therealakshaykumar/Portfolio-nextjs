69a98531feecf906c35e34bcd481851ab2b8cd09
Akshay <akshaynew45@gmail.com>
2026-05-20 11:53:01 +0530

    Added contrast

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,13 +13,13 @@ const Header = () => {
 
   return (
     <header className='!z-[999] relative'>
-        <motion.div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white/30 bg-gradient-to-b from-white/40 to-white/20 shadow-lg shadow-black/[0.08] backdrop-blur-xl sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gradient-to-b dark:from-gray-900/40 dark:to-gray-950/30 dark:border-white/10 dark:shadow-black/40'
+        <motion.div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white/20 bg-gradient-to-b from-white/20 to-white/10 shadow-lg shadow-black/[0.03] backdrop-blur-xl sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gradient-to-b dark:from-gray-900/20 dark:to-gray-950/15 dark:border-white/5 dark:shadow-black/20'
         initial={{x: "-50%" , y: -100, opacity:0}}
         animate={{x: "-50%", y:0 , opacity:1}}
         >
         </motion.div>
         <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-600 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
@@ -42,7 +42,7 @@ const Header = () => {
                   {clsx(
                     "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition-colors dark:text-gray-400 dark:hover:text-gray-300 relative",
                     {
-                      "text-gray-950 dark:text-gray-200":
+                      "text-gray-950 dark:text-white":
                         activeSection === link.name,
                     }
                   )}
@@ -60,7 +60,7 @@ const Header = () => {
                   damping: 25,
                   stiffness: 400,
                 }}
-                 className='absolute inset-0 bg-gradient-to-br from-white/40 to-white/20 rounded-full -z-10 dark:from-gray-700/40 dark:to-gray-800/30 backdrop-blur-md shadow-lg shadow-gray-900/10 dark:shadow-black/30'></motion.span>)
+                 className='absolute inset-0 bg-gradient-to-br from-white/70 to-white/50 rounded-full -z-10 dark:from-gray-600/70 dark:to-gray-700/60 backdrop-blur-md shadow-lg shadow-gray-900/20 dark:shadow-black/50'></motion.span>)
               }
             </motion.li>
           ))}
